7199d1d24c7775dc27176e6b4b490453dea52b62
Brennan Chan <99412514+BrennanChan2022@users.noreply.github.com>
2022-03-28 14:50:41 +0000

    header + setup

--- Sketch.java
+++ Sketch.java
@@ -1,13 +1,17 @@
 import processing.core.PApplet;
 
+  /**
+  * A program Sketch.java uses loop patterns to draw sequences of objects, split up into 4 evenly-sized quadrants.
+  * @author: B. Chan
+  */
+
 public class Sketch extends PApplet {
-	
 	
   /**
    * Called once at the beginning of execution, put your size all in this method
    */
   public void settings() {
-	// put your size call here
+	  // Size call
     size(400, 400);
   }
 
@@ -16,21 +20,21 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    // White background
+    background(255, 255, 255);
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
 
-    stroke(255);
-    line(50, 125, 70, 50);  
-  }
+    // Quadrant 1, 10 x 10 Grid
+
+    // Quadrant 2, 5 x grid of evenly spaced circles
+
+    // Quadrant 3, horizontal grayscale gradient
+
+    // Quadrant 4,  8 petal flower
   
-  // define other methods down here.
 }
